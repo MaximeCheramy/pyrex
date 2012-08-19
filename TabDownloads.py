b70994b1075c5af0ce509eaca5e96ef6f673cc77
Maxime Chéramy <maxime81@gmail.com>
2012-08-19 18:42:56 +0200

    Lecture fichier downloads.xml contenant la liste des téléchargements.

--- TabDownloads.py
+++ TabDownloads.py
@@ -5,7 +5,7 @@ import PyQt4.uic
 from PyQt4.QtCore import *
 from PyQt4.QtGui import QWidget, QTableWidgetItem
 
-from downloads import Download
+from downloads import Download, AnalyseDownloads
 
 class TabDownloads(QWidget):
     instance = None
@@ -18,11 +18,24 @@ class TabDownloads(QWidget):
         TabDownloads.instance = self
         self.downloads = []
 
+        self.load_downloads()
+
+    def load_downloads(self):
+        import xml.sax
+        parser = xml.sax.make_parser()
+        parser.setContentHandler(AnalyseDownloads(self.add_downloads))
+        for line in open('/home/max/.rex/downloads.xml'):
+          parser.feed(line)
+
     def add_download(self, download):
         rows = self.downloads_table.rowCount()
         self.downloads_table.insertRow(rows)
         self.downloads_table.setItem(rows, 0, QTableWidgetItem(download.file_share.name))
         self.downloads_table.setItem(rows, 1, QTableWidgetItem(download.progress))
         self.downloads_table.setItem(rows, 2, QTableWidgetItem(download.state))
-        download.start_download()
+        #download.start_download()
         self.downloads.append(download)
+
+    def add_downloads(self, downloads):
+        for download in downloads:
+          self.add_download(download)
--- downloads.py
+++ downloads.py
@@ -1,6 +1,10 @@
 # coding=utf-8
 
 from ftp import Ftp
+from Share import AnalyseShare
+from DefaultHandler import DefaultHandler
+
+from PyQt4.QtCore import QUrl
 
 class Download(object):
     def __init__(self, file_share, local_path):
@@ -29,4 +33,61 @@ class Download(object):
     def update_progress(self, readBytes, totalBytes):
         print "%d / %d" % (readBytes, totalBytes)
 
+class AnalyseDownload(object):
+    def __init__(self):
+        self.analyse_share = None
+        self.share = None
+        self.download = None
+        self.local_path = None
+
+    def open(self, name, attrs):
+        if name == "share":
+            self.analyse_share = AnalyseShare()
+
+        if self.analyse_share:
+            self.analyse_share.open(name, attrs)
+
+    def close(self, name, buf):
+        if self.analyse_share:
+            self.analyse_share.close(name, buf)
+            if name == "share":
+                self.share = self.analyse_share.share
+                self.analyse_share = None
+        elif name == "download":
+            self.download = Download(self.share, self.local_path)
+        elif name == "localpath":
+            self.local_path = buf
+        elif name == "status":
+            self.status = buf
+        elif name == "date":
+            self.date = date.fromtimestamp(int(buf) / 1000),
+
+class AnalyseDownloads(DefaultHandler):
+    def __init__(self, callback):
+        DefaultHandler.__init__(self)
+        self.downloads = None
+        self.callback = callback
+        self.analyse_download = None
+
+    def startElement(self, name, attrs):
+        DefaultHandler.startElement(self, name, attrs)
+        if name == "downloads":
+            self.downloads  = []
+        elif name == "download":
+            self.analyse_download = AnalyseDownload()
+
+        if self.analyse_download:
+            self.analyse_download.open(name, attrs)
+
+
+    def endElement(self, name):
+        if "download" in self.opened:
+            self.analyse_download.close(name, self.buf)
+            if self.analyse_download.download:
+                self.downloads.append(self.analyse_download.download)
+                self.analyse_download = None
+        else:
+            if name == "downloads":
+                self.callback(self.downloads)
+        DefaultHandler.endElement(self, name)
 
