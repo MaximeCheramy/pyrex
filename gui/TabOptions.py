907909b63755359f55787c11b74c4d812b14b4e7
Jerk <jerk@jerk-laptop.(none)>
2012-08-26 00:09:41 +0200

    Options : - Si le fichier config.ini n'existe pas, on loade une config par défaut - S'il existe on loade la config de ce fichier - On écrit dans config.ini quand on clique sur "Enregistrer"

--- gui/TabOptions.py
+++ gui/TabOptions.py
@@ -18,7 +18,7 @@ class TabOptions(QWidget):
             self.saveConfig()
         # Sinon on loade le fichier de config
         else:
-            pass
+            self.loadConfig()
 
     def saveConfig(self):
         d_conf = dict()
@@ -27,8 +27,8 @@ class TabOptions(QWidget):
         d_conf["save_dir"]          = str(self.dir_button.text())
         d_conf["max_dwl"]           = int(self.spin_max_dwl.value())
         d_conf["nb_res_page"]       = int(self.spin_nb_res_page.value())
-        d_conf["eff_dwl_init"]      = str(self.combo_eff_dwl_init.currentText())
-        d_conf["ico_notif"]         = str(self.combo_ico_notif.currentText())
+        d_conf["eff_dwl_init"]      = str(self.combo_eff_dwl_init.currentIndex())
+        d_conf["ico_notif"]         = str(self.combo_ico_notif.currentIndex())
         # Configuration Partages
         d_conf["use_FTP"]           = self.checkBox_FTP.isChecked()
         d_conf["FTP_port"]          = int(self.spin_port.value())
@@ -37,12 +37,12 @@ class TabOptions(QWidget):
         d_conf["aff_maListe"]       = self.check_aff_maListe.isChecked()
         # Configuration Avancée
         d_conf["ip_daemon"]         = str(self.edit_IP_daemon.text())
-        d_conf["logs"]              = str(self.combo_logs.currentText())
+        d_conf["logs"]              = str(self.combo_logs.currentIndex())
         d_conf["nb_ip_scan"]        = int(self.spin_nb_ip_scan.value())
-        d_conf["tps_scan"]          = str(self.edit_tps_scan.text())
+        d_conf["tps_scan"]          = int(self.spin_tps_scan.value())
         d_conf["plage_ip"]          = str(self.edit_plage_ip.text())
         d_conf["ip_conf_daemon"]    = str(self.edit_ip_conf_daemon.text())
-        d_conf["aff_myShares"]      = str(self.combo_aff_myShares.currentText())
+        d_conf["aff_myShares"]      = str(self.combo_aff_myShares.currentIndex())
         d_conf["expert_mode"]       = self.check_expert_mode.isChecked()
         # On ecrit la config dans un fichier (config.ini)
         self.writeConfig(d_conf)
@@ -57,23 +57,69 @@ class TabOptions(QWidget):
     def chooseDirectory(self):
         self.dir_button.setText(QFileDialog.getExistingDirectory(self))
         
-    def defaultConfig(self):
-        self.pseudo_edit.setText("pseudo")
-        self.dir_button.setText(os.path.expanduser('~'))
-        self.spin_max_dwl.setValue(10)
-        self.spin_nb_res_page.setValue(100)
-        self.combo_eff_dwl_init.setCurrentIndex(1)
-        self.combo_ico_notif.setCurrentIndex(0)
-        self.checkBox_FTP.setChecked(True)
-        self.spin_port.setValue(2221)
-        self.spin_connex_sim.setValue(10)
-        self.check_share_myFiles.setChecked(True)
-        self.check_aff_maListe.setChecked(False)
-        self.edit_IP_daemon.setText("127.0.0.1")
-        self.combo_logs.setCurrentIndex(1)
-        self.spin_nb_ip_scan.setValue(10)
-        self.edit_tps_scan.setText("120")
-        self.edit_plage_ip.setText("10.31.40.0-10.31.47.254")
-        self.edit_ip_conf_daemon.setText("")
-        self.combo_aff_myShares.setCurrentIndex(0)
-        self.check_expert_mode.setChecked(False)
+    def defaultConfig(self, dico=None):
+        if dico is None:
+            dico = {"pseudo"        : "pseudo", 
+                    "save_dir"      : os.path.expanduser('~'), 
+                    "max_dwl"       : 10, 
+                    "nb_res_page"   : 100, 
+                    "eff_dwl_init"  : 1, 
+                    "ico_notif"     : 0, 
+                    "use_FTP"       : True, 
+                    "FTP_port"      : 2221, 
+                    "nb_connex_sim" : 10, 
+                    "share_myFiles" : True, 
+                    "aff_maListe"   : False, 
+                    "ip_daemon"     : "127.0.0.1", 
+                    "logs"          : 1, 
+                    "nb_ip_scan"    : 10, 
+                    "tps_scan"      : 120, 
+                    "plage_ip"      : "10.31.40.0-10.31.47.254", 
+                    "ip_conf_daemon": "", 
+                    "aff_myShares"  : 0, 
+                    "expert_mode"   : False}
+        self.pseudo_edit.setText(dico["pseudo"])
+        self.dir_button.setText(dico["save_dir"])
+        self.spin_max_dwl.setValue(dico["max_dwl"])
+        self.spin_nb_res_page.setValue(dico["nb_res_page"])
+        self.combo_eff_dwl_init.setCurrentIndex(dico["eff_dwl_init"])
+        self.combo_ico_notif.setCurrentIndex(dico["ico_notif"])
+        self.checkBox_FTP.setChecked(dico["use_FTP"])
+        self.spin_port.setValue(dico["FTP_port"])
+        self.spin_connex_sim.setValue(dico["nb_connex_sim"])
+        self.check_share_myFiles.setChecked(dico["share_myFiles"])
+        self.check_aff_maListe.setChecked(dico["aff_maListe"])
+        self.edit_IP_daemon.setText(dico["ip_daemon"])
+        self.combo_logs.setCurrentIndex(dico["logs"])
+        self.spin_nb_ip_scan.setValue(dico["nb_ip_scan"])
+        self.spin_tps_scan.setValue(dico["tps_scan"])
+        self.edit_plage_ip.setText(dico["plage_ip"])
+        self.edit_ip_conf_daemon.setText(dico["ip_conf_daemon"])
+        self.combo_aff_myShares.setCurrentIndex(dico["aff_myShares"])
+        self.check_expert_mode.setChecked(dico["expert_mode"])
+        
+    def loadConfig(self):
+        # On charge la config
+        config = open("config.ini", "r")
+        dico = dict()
+        # On la parse et on met les clés et valeurs dans un dico
+        for line in config:
+            if "=" in line:
+                key, value = line.split("=", 1)
+                value = value[:-1]
+                # Exception : integer
+                try:
+                    value = int(value)
+                except ValueError:
+                    pass
+                # Exception : boolean
+                if value == 'True':
+                    value = True
+                elif value == 'False':
+                    value = False
+                dico[key] = value
+            else:
+                pass
+        config.close()
+        # On envoie la config dans Rex
+        self.defaultConfig(dico)
