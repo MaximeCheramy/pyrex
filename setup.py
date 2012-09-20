575e9d5c432879eeb8f4195efcf048da3db604df
Jerk31 <lebourrin31@hotmail.fr>
2012-09-20 16:14:21 +0200

    Petite erreur dans le setup :)

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 import py2exe 
 
 setup(
-	windows=[{"script":"icon.py"}],
+	windows=[{"script":"pyrex.py"}],
 	options={"py2exe":{"includes":["sip", "PyQt4.QtCore", "PyQt4._qt"]}}
 )
