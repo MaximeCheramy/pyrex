8d30e8d7407ad98b794903120e1a21c1ccd74a5f
Jerk31 <lebourrin31@hotmail.fr>
2012-09-25 18:49:40 +0200

    Modif du fichier de package

--- update_package.sh
+++ update_package.sh
@@ -9,28 +9,34 @@ if [ ! -d pyRex ]; then
     mkdir -p pyRex/DEBIAN/
     mkdir -p pyRex/usr/share/pyrex/
     mkdir -p pyRex/usr/bin/
-    echo "#!/bin/sh
-exec python /usr/share/pyrex/pyrex.py " > pyRex/usr/bin/pyrex
-    echo '#!/bin/bash
-sudo rm -r /usr/share/pyrex
-sudo rm /usr/bin/pyrex' > pyRex/DEBIAN/prerm
-    chmod 755 pyRex/DEBIAN/*
+    cat > pyRex/usr/bin/pyrex << EOF
+#!/bin/sh
+exec python /usr/share/pyrex/pyrex.py
+EOF
+    cat > pyRex/DEBIAN/prerm << EOF
+sudo rm -rf /usr/share/pyrex/
+sudo rm /usr/bin/pyrex
+EOF
+    chmod +x pyRex/usr/bin/pyrex
 fi
 
 ## On balance les infos dans le paquet
 cd pyRex
 cp -r ../pyrex/* usr/share/pyrex/
 rm usr/share/pyrex/*.pyc usr/share/pyrex/*~ usr/share/pyrex/gui/*.pyc usr/share/pyrex/gui/*~
-echo "Package: pyrex
-Version: "$version"
+cat > DEBIAN/control << EOF
+Package: pyrex
+Version: $version
 Section: Python
 Priority: optional
 Architecture: all
-Depends: bash, python (= 2.7), python-qt4 (>= 4.7), rex
+Depends: python (>= 2.7), python (<< 2.8), python-qt4 (>= 4.7), rex
 Maintainer: J. SIVADIER <sivadier@etud.insa-toulouse.fr> and M. CHERAMY <mcheramy@etud.insa-toulouse.fr>
 Homepage: http://etud.insa-toulouse.fr/~rex
-Description: Client FTP pour les étudiants de l'INSA de Toulouse codé en python. Successeur de Rex." > DEBIAN/control
+Description: Client FTP pour les étudiants de l'INSA de Toulouse codé en python. Compatible avec Rex Daemon.
+EOF
 find usr -exec md5sum {} + > DEBIAN/md5sums
+chmod 755 DEBIAN/*
 
 ## On compile
 cd ../
