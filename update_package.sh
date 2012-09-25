435730590f53bd7cda94316cd00a14f2e63ef34b
Jerk31 <lebourrin31@hotmail.fr>
2012-09-25 17:28:25 +0200

    Ajout du fichier pour créer le package debian Use : Placer au dessus de pyrex et faire ./update...

---
+++ update_package.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+## Copier le dossier python de git dans le même répertoire! ##
+
+echo "entrez la version exacte du paquet"
+read version
+
+## Creation de la base du paquet si ça n'existe pas
+if [ ! -d pyRex ]; then
+    mkdir -p pyRex/DEBIAN/
+    mkdir -p pyRex/usr/share/pyrex/
+    echo '#!/bin/bash
+# Create the pyrex executable
+echo "#!/bin/sh
+exec python /usr/share/pyrex/pyrex.py " > /usr/bin/pyrex' > pyRex/DEBIAN/postinst
+    echo '#!/bin/bash
+sudo rm -r /usr/share/pyrex
+sudo rm /usr/bin/pyrex' > pyRex/DEBIAN/prerm
+    chmod 755 pyRex/DEBIAN/*
+fi
+
+## On balance les infos dans le paquet
+cd pyRex
+cp -r ../pyrex/* usr/share/pyrex/
+rm usr/share/pyrex/*.pyc usr/share/pyrex/*~ usr/share/pyrex/gui/*.pyc usr/share/pyrex/gui/*~
+echo "Package: pyrex
+Version: "$version"
+Section: Python
+Priority: optional
+Architecture: all
+Depends: bash, python (= 2.7), python-qt4 (>= 4.7)
+Maintainer: J. SIVADIER <sivadier@etud.insa-toulouse.fr> and M. CHERAMY <mcheramy@etud.insa-toulouse.fr>
+Homepage: http://etud.insa-toulouse.fr/~rex
+Description: Client FTP pour les étudiants de l'INSA de Toulouse codé en python. Successeur de Rex." > DEBIAN/control
+find usr -exec md5sum {} + > DEBIAN/md5sums
+
+## On compile
+cd ../
+dpkg-deb --build pyRex
+
+## On change le nom
+mv pyRex.deb pyRex_$version.deb
